35e22bef171c1169d9919227ccb1dfca206e4b57
Eva Smyth <106627495+evaSmyth@users.noreply.github.com>
2023-12-07 09:23:44 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ const hamburgerBtn  = document.querySelector('.hamburg-btn');
 const navList = document.querySelector('.main-nav');
 const removeNav = document.querySelector('.remove-nav');
 const main = document.querySelector('.main');
+const footer = document.querySelector('.footer-container')
+const footerBand = document.querySelector('.top-footer-band')
 
 const showNav = function() {
     navList.classList.add('nav-bar-show');
-    //main.classList.add('blur')
+    main.classList.add('blur');
+    footer.classList.add('blur');
+    footerBand.classList.add('blur');
 };
 const hideNav = function() {
+    footer.classList.remove('blur');
+    main.classList.remove('blur');
+    footerBand.classList.remove('blur');
     navList.classList.remove('nav-bar-show');
     aboutMeDropdown.classList.remove('show-dropdown');
     trainingDropdown.classList.remove('show-dropdown');
     shopDropdown.classList.remove('show-dropdown');
-    //main.classList.remove('blur')
-}
+    
+};
 hamburgerBtn.addEventListener('click',showNav);
 removeNav.addEventListener('click', hideNav);
 
